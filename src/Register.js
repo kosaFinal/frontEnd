b292cf4975b82dcf2d4634173ecd4186e33bf241
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-11-04 18:54:22 +0900

    chore: 회원가입 페이지 수정

--- src/Register.js
+++ src/Register.js
@@ -1,44 +1,56 @@
 import React from "react";
 import "./Register.css";
-import RadioGroup from "./RadioGroup";
 import Radio from "./Radio";
+import Header from "./Header";
 
 function Register() {
   return (
-    <div className="register-container">
-      
-      <div className="register-box">
-      <div className="logo-section">
-        <div className="logo-icon">
-          <img src="/assets/logo_reimg.png" alt="이미지" />
+    <register>
+      <Header />
+      <div className="register">
+        <h1 className="register-title">회원가입</h1>
+        <div className="register-container">
+          <div className="register-box">
+            <h1 className="register-intro">안녕하세요 카페인입니다.</h1>
+            <div className="register-radio">
+              <label className="usertype-label" value="회원유형">
+                회원 유형
+              </label>
+
+              <Radio
+                className="register-radio-button"
+                name="userType"
+                value="user"
+                defaultchecked
+              >
+                USER
+              </Radio>
+
+              <Radio
+                className="register-radio-button"
+                name="userType"
+                value="manager"
+                defaultchecked
+              >
+                MANAGER
+              </Radio>
+            </div>
+            <div className="id-section">
+              <input type="text" placeholder="아이디" />
+              <button>확인</button>
+            </div>
+
+            <input type="text" placeholder="성 명" />
+
+            <input type="password" placeholder="비밀번호" />
+            <input type="password" placeholder="비밀번호 확인" />
+            <div className="register-button">
+              <button>회원가입</button>
+            </div>
+          </div>
         </div>
       </div>
-        <div className="register-radio">
-          <label className="usertype-label" value="회원유형">회원 유형</label>
-
-            <Radio className="register-radio-button" name="userType" value="user" defaultchecked>
-            USER
-            </Radio>
-
-          <Radio className="register-radio-button" name="userType" value="manager" defaultchecked>
-            MANAGER
-          </Radio>
-          
-        </div>
-        <div className="id-section">
-          <input type="text" placeholder="아이디" />
-          <button>확인</button>
-        </div>
-
-        <input type="text" placeholder="성 명" />
-
-        <input type="password" placeholder="비밀번호" />
-        <input type="password" placeholder="비밀번호 확인" />
-        <div className="register-button">
-          <button>회원가입</button>
-        </div>
-      </div>
-    </div>
+    </register>
   );
 }
 
