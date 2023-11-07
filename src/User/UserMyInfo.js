6d1c09095c16374b0146fc2f81f27af1e2435635
ckdud9299 <ckdud9299@naver.com>
2023-11-07 19:44:47 +0900

    feat : UserMyInfo 수정

--- src/User/UserMyInfo.js
+++ src/User/UserMyInfo.js
@@ -1,9 +1,32 @@
+import Footer from "../Footer";
+import UserNav from "./UserNav";
+import "./UserMyInfo.css";
+
 const UserMyInfo = () => {
   return (
     <usermyinfo>
-      <div>
-        <p>고객 예약 현황 페이지입니다.</p>
+      <UserNav />
+      <div className="myUserInfo-container">
+        <div className="myUserInfo-title">
+          <p>내 정보 조회</p>
+        </div>
+        <hr />
+        <div className="myUserInfo-inform">
+          <p>
+            <span>개인 정보 보호</span>를 위해
+            <br />
+            비밀번호를 입력해주세요
+          </p>
+        </div>
+        <div className="myUserInfo-input">
+          <label>비밀번호</label>
+          <input type="password" />
+        </div>
+        <div className="myUserInfo-button">
+          <button>확인하기</button>
+        </div>
       </div>
+      <Footer />
     </usermyinfo>
   );
 };
