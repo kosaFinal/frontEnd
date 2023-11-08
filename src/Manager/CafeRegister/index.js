381faa74361116f60c39358f7bd883a3915663c6
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-11-08 12:08:18 +0900

    chore: 카페등록페이지 디자인 변경

--- src/Manager/CafeRegister/index.js
+++ src/Manager/CafeRegister/index.js
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import ManagerNav from "../ManagerNav";
-import "./cafeRegister.css";import Footer from "../../Footer";
-;
-
-
+import "./cafeRegister.css";
+import Footer from "../../Footer";
+import CafeType from "./CafeType";
 const CafeRegister = () => {
-  const [number,setNumer] = useState(1);
+  const [number, setNumer] = useState(1);
 
-  const addNumber = () =>{
+  const addNumber = () => {
     setNumer((prevNumber) => {
-      if(prevNumber >= 6){
+      if (prevNumber >= 6) {
         return 6;
       }
       return prevNumber + 1;
     });
   };
 
-  const subNumber = () =>{
+  const subNumber = () => {
     setNumer((prevNumber) => {
-      if(prevNumber <= 1){
+      if (prevNumber <= 1) {
         return 1;
       }
       return prevNumber - 1;
@@ -27,24 +26,31 @@ const CafeRegister = () => {
 
   return (
     <caferegister>
-      <ManagerNav/>
+      <ManagerNav />
       <div className="register-full-container">
         <div className="register-number">
-          <span className="stepNumber">
-            0{number}
-          </span>
-          <span className="totalNumber">
-           / 06
-          </span>
+          <span className="stepNumber">0{number}</span>
+          <span className="totalNumber">/ 06</span>
         </div>
         <div className="button-container">
-        <div className="register-buttons">
-          <button className="previous" onClick={subNumber}>이전</button>
-          <button className="next" onClick={addNumber}>다음</button>
-        </div>
+          {/* <div className="register-buttons"> */}
+            <div className="button-previous">
+            <button className="previous" onClick={subNumber}>
+            {"<"}
+            </button>
+            </div>
+            <div className="register-stepbox">
+              <CafeType />
+            </div>
+            <div className="button-next">
+            <button className="next" onClick={addNumber}>
+              {">"}
+            </button>
+            </div>
+          {/* </div> */}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </caferegister>
   );
 };
