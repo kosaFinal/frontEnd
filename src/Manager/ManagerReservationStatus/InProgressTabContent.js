c2847989a09de7d20181d375ed6f6fcf9817830c
ckdud9299 <58415917+ckdud9299@users.noreply.github.com>
2023-11-18 10:11:52 +0900

    chore : 점주 진행중인 예약 탭 버튼 추가

--- src/Manager/ManagerReservationStatus/InProgressTabContent.js
+++ src/Manager/ManagerReservationStatus/InProgressTabContent.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import CancleModal from "./CancleModal";
-import "./InProgressTabContent.css";
+import FinishMoel from './FinishModal';
+import "./StatusTabContent.css";
 
 const InProgressTabContent = () => {
   const inProgressReservations = [
-    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'A1' },
-    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'B2' },
-    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'C3' }
+    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'A1', date: '2023-11-18' },
+    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'B2', date: '2023-11-18' },
+    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'C3', date: '2023-11-18' }
   ];
 
   const [isCancleModalOpen, setIsCancleModalOpen] = useState(false);
@@ -25,22 +26,50 @@ const InProgressTabContent = () => {
     handleCloseCancleModal();
   };
 
+  const [isFinishModalOpen, setIsFinishModalOpen] = useState(false);
+
+  const handleOpenFinishModal = () => {
+    setIsFinishModalOpen(true);
+  };
+
+  const handleCloseFinishModal = () => {
+    setIsFinishModalOpen(false);
+  };
+
+  const handleReservationFinish = () => {
+    // 예약 종료 로직
+    handleCloseFinishModal();
+  };
+
   return (
     <div>
       {inProgressReservations.map((reservation, index) => (
           <div className="reservation-item">
-            <div className="inprogress-name">{reservation.name}</div>
-            <div className="inprogress-info">
+            <div className="reservation-name">{reservation.name}</div>
+            <div className="reservation-info">
               <div>예약 테이블: {reservation.type}</div>
               <div>예약 좌석: {reservation.seat}</div>
               <div>인원수: {reservation.cnt}</div>
+              <div>예약 날짜: {reservation.date}</div>
               <div>예약 시간: {reservation.time}</div>
             </div>
-            <div className="inprogress-button">
-              <button onClick={() => handleOpenCancleModal()}>취소하기</button>
+            <div className="reservation-button">
+              <button onClick={() => handleOpenFinishModal()}>예약 종료</button>
+              <button onClick={() => handleOpenCancleModal()}>예약 취소</button>
             </div>
           </div>
       ))}
+      {isFinishModalOpen && (
+        <div className="backdrop"></div>
+      )}
+      {isFinishModalOpen && (
+        <FinishMoel
+          isOpen={isFinishModalOpen}
+          onClose={handleCloseFinishModal}
+          onConfirm={handleReservationFinish}
+        />
+      )}
+
       {isCancleModalOpen && (
         <div className="backdrop"></div>
       )}
--- src/Manager/ManagerReservationStatus/UpcomingTabContent.js
+++ src/Manager/ManagerReservationStatus/UpcomingTabContent.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import "./UpcomingTabContent.css";
+import "./StatusTabContent.css";
 import CancleModal from "./CancleModal";
 import ConfirmModal from "./ConfirmModal";
 
 const UpcomingTabContent = () => {
   const upComingReservations = [
-    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'A1' },
-    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'B2' },
-    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'C3' }
+    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'A1', date: '2023-11-18' },
+    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'B2', date: '2023-11-18' },
+    { name: '윤형우', type: '4인석', time: '12:00 ~ 15:00', cnt: '3명', seat: 'C3', date: '2023-11-18' }
   ];
 
   const [isCancleModalOpen, setIsCancleModalOpen] = useState(false);
@@ -45,26 +45,23 @@ const UpcomingTabContent = () => {
 
   return (
     <div>
-      {/* <hr className="divider" /> */}
       {upComingReservations.map((reservation, index) => (
-        <div key={index}>
           <div className="reservation-item">
-          <div className="upcoming-name">{reservation.name}</div>
-          <div className="upcoming-info">
-            <div>예약 테이블: {reservation.type}</div>
-            <div>예약 좌석: {reservation.seat}</div>
-            <div>인원수: {reservation.cnt}</div>
-            <div>예약 시간: {reservation.time}</div>
-          </div>
-          <div className="upcoming-button">
-          <button onClick={() => handleOpenConfirmModal()}>확정하기</button>
-              <button onClick={() => handleOpenCancleModal()}>취소하기</button>
-          </div>
+            <div className="reservation-name">{reservation.name}</div>
+            <div className="reservation-info">
+              <div>예약 테이블: {reservation.type}</div>
+              <div>예약 좌석: {reservation.seat}</div>
+              <div>인원수: {reservation.cnt}</div>
+              <div>예약 날짜: {reservation.date}</div>
+              <div>예약 시간: {reservation.time}</div>
+            </div>
+            <div className="reservation-button">
+              <button onClick={() => handleOpenConfirmModal()}>예약 확정</button>
+              <button onClick={() => handleOpenCancleModal()}>예약 취소</button>
+            </div>
         </div>
-        {/* {index < upComingReservations.length - 1 && <hr className="divider" />} */}
-      </div>
       ))}
-      {/* <hr className="divider" /> */}
+      <hr className="divider" />
       {isConfirmModalOpen && (
         <div className="backdrop"></div>
       )}
