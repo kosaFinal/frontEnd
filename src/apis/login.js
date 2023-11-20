4edf7f8f43bef405ced695cf8c34922c81ef0a27
yoonhyungwoo <guddn13212@naver.com>
2023-11-20 17:24:09 +0900

    chore : infoUpdate 수정

--- src/apis/login.js
+++ src/apis/login.js
@@ -5,6 +5,6 @@ axios.defaults.baseURL = "http://localhost:8080";
 export function checkPassword(password) {
   return axios.post("/user/check/password", { password });
 }
-export function updatePassword(data) {
-  return axios.patch("/user/update/password", { data });
+export function updatePassword() {
+  return axios.patch("/user/update/password", { password: "password" });
 }
