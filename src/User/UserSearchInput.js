0e00030e2024cf9871c947179b294c1cb39efcbe
yoonhyungwoo <guddn13212@naver.com>
2023-11-27 18:32:10 +0900

    style : Search css 수정

--- src/User/UserSearchInput.js
+++ src/User/UserSearchInput.js
@@ -327,19 +327,21 @@ const UserSearchInput = ({
             ))}
           </div>
         )}
-        {!parentResults && (
-          <div className="user_search_pagination">
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button
-                key={index + 1}
-                onClick={() => handlePageChange(index + 1)}
-                className={currentPage === index + 1 ? "active" : ""}
-              >
-                {index + 1}
-              </button>
-            ))}
-          </div>
-        )}
+        <div className="page_white">
+          {!parentResults && (
+            <div className="user_search_pagination">
+              {Array.from({ length: totalPages }, (_, index) => (
+                <button
+                  key={index + 1}
+                  onClick={() => handlePageChange(index + 1)}
+                  className={currentPage === index + 1 ? "active" : ""}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
         {showInfo && (
           <div className="searchcafeinfo">
             <UserSearchCafeInfo
