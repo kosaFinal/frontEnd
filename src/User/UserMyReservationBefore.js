e471ab852cdad4f715dd1bf120170977e48066d8
yoonhyungwoo <guddn13212@naver.com>
2023-12-06 19:37:29 +0900

    hotfix! : css 수정

--- src/User/UserMyReservationBefore.js
+++ src/User/UserMyReservationBefore.js
@@ -30,7 +30,7 @@ const UserMyReservationBefore = () => {
       } catch (error) {
         console.error("잘좀 해봐 : ", error);
       } finally {
-       setLoading(false);
+        setLoading(false);
       }
     };
     reservationBeforeInfo();
@@ -39,70 +39,68 @@ const UserMyReservationBefore = () => {
   return (
     <usermyreservationbefore>
       <div className="user_myReservation-container_before">
-        {loading? (
+        {loading ? (
           <div className="user_myReservation_loading">
             <p>예약 내역을 불러오는 중입니다</p>
-            <PulseLoader
-              color="#CCC"
-              margin={5}
-              speedMultiplier={0.8}
-            />
+            <PulseLoader color="#CCC" margin={5} speedMultiplier={0.8} />
           </div>
         ) : (
           <>
             {reservationBefore.length === 0 ? (
-          <div className="user_myReservation_no_exist">
-            예약 현황이 없습니다.
-          </div>
-        ) : (
-          <>
-            {paginate(reservationBefore).map((reservation, index) => (
-              <div key={index} className="user_reservation-item">
-                <div className="user_reservation_cafe_img">
-                  <img src={`data:image/;base64,${reservation.cafeRepImg}`} />
-                </div>
-
-                <div className="user_reservation_text_before">
-                  <div className="user_cafe_name_before">
-                    {reservation.cafeName}
-                  </div>
-                  <div className="user_reservation_row_before">
-                    <div className="user_inprogress-info_before">
-                      <p>
-                        {" "}
-                        이용 날짜 : {reservation.reserveDate}
-                        <br />
-                        이용 시간 : {reservation.reserveStart} ~{" "}
-                        {reservation.reserveEnd}
-                        <br />
-                        좌석: {reservation.tableNumber}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                {reservation.state === "A" && (
-                  <div className="user_reservation_application_before">
-                    <h4>승인 중</h4>
-                  </div>
-                )}
-                {reservation.state === "P" && (
-                  <div className="user_reservation_progress_before">
-                    <h4>이용 중</h4>
-                  </div>
-                )}
+              <div className="user_myReservation_no_exist">
+                예약 현황이 없습니다.
               </div>
-            ))}
-            <ReactPaginate
-              activePage={currentPage}
-              itemsCountPerPage={itemsPerPage}
-              totalItemsCount={reservationBefore.length}
-              pageRangeDisplayed={5}
-              onChange={handlePageChange}
-              prevPageText={"‹"}
-              nextPageText={"›"}
-            />
-          </>
-        )}
+            ) : (
+              <>
+                {paginate(reservationBefore).map((reservation, index) => (
+                  <div key={index} className="user_reservation-item">
+                    <div className="user_reservation_cafe_img">
+                      <img
+                        src={`data:image/;base64,${reservation.cafeRepImg}`}
+                      />
+                    </div>
+
+                    <div className="user_reservation_text_before">
+                      <div className="user_cafe_name_before">
+                        {reservation.cafeName}
+                      </div>
+                      <div className="user_reservation_row_before">
+                        <div className="user_inprogress-info_before">
+                          <p>
+                            {" "}
+                            이용 날짜 : {reservation.reserveDate}
+                            <br />
+                            이용 시간 : {reservation.reserveStart} ~{" "}
+                            {reservation.reserveEnd}
+                            <br />
+                            좌석: {reservation.tableNumber}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    {reservation.state === "A" && (
+                      <div className="user_reservation_application_before">
+                        <h4>승인 전</h4>
+                      </div>
+                    )}
+                    {reservation.state === "P" && (
+                      <div className="user_reservation_progress_before">
+                        <h4>승인 완료</h4>
+                      </div>
+                    )}
+                  </div>
+                ))}
+                <ReactPaginate
+                  activePage={currentPage}
+                  itemsCountPerPage={itemsPerPage}
+                  totalItemsCount={reservationBefore.length}
+                  pageRangeDisplayed={5}
+                  onChange={handlePageChange}
+                  prevPageText={"‹"}
+                  nextPageText={"›"}
+                />
+              </>
+            )}
           </>
         )}
       </div>
