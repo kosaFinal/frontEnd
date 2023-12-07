003dfe85784b922d57dc24fcbad913f6f26b4779
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-12-07 14:12:45 +0900

    chore: api 수정

--- src/apis/UserReservation.js
+++ src/apis/UserReservation.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-axios.defaults.baseURL = "http://27.96.134.5:8080";
+axios.defaults.baseURL = "https://jieunyes.shop/api";
 
 export function readTableList(cafeId) {
   return axios.get(`/user/reservation/cafe/${cafeId}`);
